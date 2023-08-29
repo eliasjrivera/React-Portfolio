dfb9cfd40ac21329ce4b5ca81b7bf3f08cf0ce77
Elias Rivera <eliasjohnrivera@gmail.com>
2023-08-28 18:26:47 -0700

    added input into contact tab

--- React Portfolio/src/components/pages/Contact.jsx
+++ React Portfolio/src/components/pages/Contact.jsx
@@ -1,24 +1,91 @@
 // switch name to Contact after website functions
 // Blog = Contact
+import { useState } from 'react';
+// import './style.css';
+
+import { checkPassword, validateEmail } from '../../utils/helpers';
+
 export default function Contact() {
+    // Create state variables for the fields in the form
+    // We are also setting their initial values to an empty string
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleInputChange = (e) => {
+        // Getting the value and name of the input which triggered the change
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+
+        // Based on the input type, we set the state of either name, email, and message
+        if (inputType === 'name') {
+        setName(inputValue);
+        } else if (inputType === 'email') {
+        setEmail(inputValue);
+        } else {
+        setMessage(inputValue);
+        }
+    };
+
+    const handleFormSubmit = (e) => {
+        // Preventing the default behavior of the form submit (which is to refresh the page)
+        e.preventDefault();
+
+        // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+        // || !userName
+        if (!validateEmail(email)) {
+        setErrorMessage('Email is invalid');
+        // We want to exit out of this code block if something is wrong so that the user can correct it
+        return;
+        // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
+        }
+        // if (!checkPassword(password)) {
+        // setErrorMessage(
+        //     `Choose a more secure password for the account: ${userName}`
+        // );
+        // return;
+        // }
+        // alert(`Hello ${userName}`);
+
+        // If everything goes according to plan, we want to clear out the input after a successful registration.
+        setEmail('');
+        setMessage('');
+        setName('');
+    };
+
     return (
-        <div>
-            <h1>Contact</h1>
-            <p>
-                Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-                Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-                dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-                sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-                sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-                vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-                libero. Class aptent taciti sociosqu ad litora torquent per conubia
-                nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-                lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-                Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-                in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-                bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-                est ligula suscipit velit, sed bibendum turpis dui in sapien.
-            </p>
+        <div className="container text-center">
+            <form className="form" onSubmit={handleFormSubmit}>
+                <input
+                    value={name}
+                    name="name"
+                    onChange={handleInputChange}
+                    type="name"
+                    placeholder="name"
+                />
+                <input
+                    value={email}
+                    name="email"
+                    onChange={handleInputChange}
+                    type="text"
+                    placeholder="email"
+                />
+                <input
+                    value={message}
+                    name="message"
+                    onChange={handleInputChange}
+                    type="message"
+                    placeholder="message"
+                    />
+                <button type="submit">Submit</button>
+            </form>
+            {errorMessage && (
+                <div>
+                    <p className="error-text">{errorMessage}</p>
+                </div>
+            )}
         </div>
     );
 }
